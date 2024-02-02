ba87b86ec123bcc3e4693ffd9ddfe98e47eb30b4
CodeSandbox <support@codesandbox.io>
2024-02-02 16:49:53 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+const phrases = [
+  "No",
+  "Are you sure?",
+  "Really sure?",
+  "Hebe please?",
+  "Don't do this to me",
+  "I'm gonna cry",
+  "You are breaking my heart ;(",
+];
 
 function App() {
   const [count, setCount] = useState(0);
+  const [yesPressed, setYesPressed] = usseState(false);
+  const yesButtonSizen = noCount * 20 + 16;
+
+  function handleNoClick() {
+    setNoCount(noCount + 1);
+  }
+
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, phrases.length - 1)]
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div classNane="valentine-container">
+      {yesPressed ? (
+        <>
+          <img src="https://media1.tenor.com/m/J3o-7E-2JGAAAAAd/emuahh.gif" alt="bears kissing" />
+          <div className="text">Yay!!!</div>
+          </>
+      ) : (
+        <>
+          <img src="https://media1.tenor.com/m/i-EiA87Hy4QAAAAd/kiss-day.gif" alt="bears with hearts" />
+          <div>Will you be my valentine?</div>
+          <div>
+            <button 
+              className="yesButton"
+              style {{ fontsize: yesButtonSize }}
+              oneClick{() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button oneClick={handleNoClick} className="noBurron">
+              {getNoButtonText()}
+            </button>
+          </div>
+        </>
+    )}
+    </div>
   );
+}
+
+export default App;
+      
+
+  return <></>;
 }
 
 export default App;
